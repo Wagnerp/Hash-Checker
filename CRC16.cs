d1f2fea10dcad18be05527feb358285eb6ea98a6
Anthony Perez <anthony@amazingant.com>
2012-10-03 11:17:18 -0400

    Added CRC16.cs to project, will generate hash checksums

---
+++ CRC16.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crypto = System.Security.Cryptography;
+
+namespace System.Security.Cryptography
+{
+    class CRC16Generator : Crypto.HashAlgorithm
+    {
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override byte[] HashFinal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
