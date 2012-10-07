cf3c82b8f5b61cf6c1ec288812017d92c52edb16
Anthony Perez <anthony@amazingant.com>
2012-10-07 19:04:43 -0400

    More rearranging, and new features as mentioned:
    Moved generation of the hashes to a new method GenerateHash; added an enum InputResponseTypes with Quit, Continue, and Hash Provided -- quit quits, continue generates and displays all available hash types, and HashProvided checks the new value against the appropriate type; as a sub-feature to the new ability to generate and display all possible hash types, typing in "copy" after seeing the hashes puts all the values into your clipboard for later.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,14 @@ namespace Hash_Checker
             // Supported types are stored here by their character-length when encoded as a hex string
             MD5 = 32,
             SHA1 = 40,
-            SHA512 = 128
+            SHA512 = 128,
+            Unknown = -1
+        }
+        public enum InputResponseTypes
+        {
+            Quit = -1,
+            Continue = 0,
+            HashProvided = 1
         }
 
         [STAThreadAttribute]
@@ -44,25 +51,37 @@ namespace Hash_Checker
                     return;
                 }
                 // Check the clipboard for a checksum we can use
-                if (!checkClipboard(ref expectedHash))
+                InputResponseTypes ourResponse = checkClipboard(ref expectedHash);
+                if (ourResponse == InputResponseTypes.Quit)
                     return;
+                if (ourResponse == InputResponseTypes.Continue)
+                {
+                    string hashes = "";
+                    foreach (SupportedHashes hashType in Enum.GetValues(typeof(SupportedHashes)))
+                    {
+                        hashes += hashType.ToString() + ": " + GenerateHash(fileToHash, hashType) + "\n";
+                    }
+                    Console.Write(hashes);
+                    if (Console.ReadLine() == "copy")
+                        Winforms.Clipboard.SetText(hashes);
+                    return;
+                }
             }
             string HashUsed = "";
-            Crypto.HashAlgorithm hashGenerator;
-            byte[] hashbytes;
+            string checksum = "";
             if (expectedHash.Length == (int)SupportedHashes.MD5)
             {
-                hashGenerator = Crypto.MD5.Create();
                 HashUsed = "MD5";
+                checksum = GenerateHash(fileToHash, SupportedHashes.MD5);
             }
             else if (expectedHash.Length == (int)SupportedHashes.SHA1)
             {
-                hashGenerator = Crypto.SHA1.Create();
+                checksum = GenerateHash(fileToHash, SupportedHashes.SHA1);
                 HashUsed = "SHA1";
             }
             else if (expectedHash.Length == (int)SupportedHashes.SHA512)
             {
-                hashGenerator = Crypto.SHA512.Create();
+                checksum = GenerateHash(fileToHash, SupportedHashes.SHA512);
                 HashUsed = "SHA512";
             }
             else
@@ -71,24 +90,21 @@ namespace Hash_Checker
                 Console.ReadLine();
                 return;
             }
-            // Make the checksum and build it into the StringBuilder as hex
-            StringBuilder checksumBuilder = new StringBuilder();
-            hashbytes = hashGenerator.ComputeHash(System.IO.File.ReadAllBytes(fileToHash));
-            foreach (byte chunk in hashbytes) checksumBuilder.Append(chunk.ToString("x2"));
             // Check the values
-            if (checksumBuilder.ToString().ToLower() == expectedHash.ToLower())
+            if (checksum.ToLower() == expectedHash.ToLower())
                 Console.WriteLine("Hash provided matches calculated hash.");
             else
                 Console.WriteLine("Hash provided DOES NOT match calculated hash.");
             Console.WriteLine("Hash Method: " + HashUsed);
             Console.WriteLine();
             Console.WriteLine("Expected Hash: " + expectedHash.ToLower());
-            Console.WriteLine("Result Hash:   " + checksumBuilder.ToString().ToLower());
+            Console.WriteLine("Result Hash:   " + checksum.ToLower());
             Console.ReadLine();
         }
 
-        private static bool checkClipboard(ref string expectedHash)
+        private static InputResponseTypes checkClipboard(ref string expectedHash)
         {
+            string lastInput = "";
             do
             {
                 if (Winforms.Clipboard.ContainsText())
@@ -96,21 +112,52 @@ namespace Hash_Checker
                     // Grab clipboard text
                     expectedHash = Winforms.Clipboard.GetText();
                     // Check for quit or just q
-                    if (expectedHash == "quit" || expectedHash == "q")
-                        return false;
+                    if (lastInput == "quit" || lastInput == "q")
+                        return InputResponseTypes.Quit;
+                    if (lastInput == "go" || lastInput == "continue")
+                        return InputResponseTypes.Continue;
                     // See if the length matches any supported hash types
                     foreach (SupportedHashes hashType in Enum.GetValues(typeof(SupportedHashes)))
                     {
                         if (expectedHash.Length == (int)hashType)
-                            return true;
+                            return InputResponseTypes.HashProvided;
                     }
                     // If no matches, clear our value
                     expectedHash = "";
                 }
                 // Let the user know to copy the hash
                 Console.WriteLine("Please select and copy the hash to compare the file to.");
-                Console.ReadLine();
+                lastInput = Console.ReadLine();
             } while (true);
+        }
+
+        private static string GenerateHash(string filePath, SupportedHashes HashType)
+        {
+            if (!System.IO.File.Exists(filePath))
+                return "";
+
+            Crypto.HashAlgorithm hashGenerator;
+            byte[] hashbytes;
+            switch (HashType)
+            {
+                case SupportedHashes.MD5:
+                    hashGenerator = Crypto.MD5.Create();
+                    break;
+                case SupportedHashes.SHA1:
+                    hashGenerator = Crypto.SHA1.Create();
+                    break;
+                case SupportedHashes.SHA512:
+                    hashGenerator = Crypto.SHA512.Create();
+                    break;
+                default:
+                    return "";
+            }
+
+            // Make the checksum and build it into the StringBuilder as hex
+            StringBuilder checksumBuilder = new StringBuilder();
+            hashbytes = hashGenerator.ComputeHash(System.IO.File.ReadAllBytes(filePath));
+            foreach (byte chunk in hashbytes) checksumBuilder.Append(chunk.ToString("x2"));
+            return checksumBuilder.ToString();
         }
     }
 }
